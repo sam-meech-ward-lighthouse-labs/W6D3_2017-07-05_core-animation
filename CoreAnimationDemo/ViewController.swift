30d0eb108f380331c280873d95d02d1b232a7cfa
Sam Meech-Ward <sam@meech-ward.com>
2017-07-05 09:22:24 -0700

    Initial Commit

---
+++ CoreAnimationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreAnimationDemo
+//
+//  Created by Sam Meech-Ward on 2017-07-05.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
